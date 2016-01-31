b96dc35893601d564702d1d82a98f63014067b30
Kev Rowe <kev.rowe@gmail.com>
2016-01-31 20:30:37 +0000

    add touch-velocity

---
+++ public/js/touch-velocity.js
@@ -0,0 +1,40 @@
+/**
+  Zach Smith's touch-velocity
+  url: https://www.npmjs.com/package/touch-velocity
+  license: MIT (http://opensource.org/licenses/MIT)
+*/
+
+function Velocity() {
+  this.positionQueue = []
+  this.timeQueue = []
+}
+
+Velocity.prototype.reset = function() {
+  this.positionQueue.splice(0)
+  this.timeQueue.splice(0)
+}
+
+Velocity.prototype.pruneQueue = function(ms) {
+  //pull old values off of the queue
+  while(this.timeQueue.length && this.timeQueue[0] < (Date.now() - ms)) {
+    this.timeQueue.shift()
+    this.positionQueue.shift()
+  }
+}
+
+Velocity.prototype.updatePosition = function(position) {
+  this.positionQueue.push(position)
+  this.timeQueue.push(Date.now())
+  this.pruneQueue(50)
+}
+
+Velocity.prototype.getVelocity = function() {
+  this.pruneQueue(1000)
+  var length = this.timeQueue.length
+  if(length < 2) return 0
+
+  var distance = this.positionQueue[length-1] - this.positionQueue[0]
+    , time = (this.timeQueue[length-1] - this.timeQueue[0]) / 1000
+
+  return distance / time
+}
